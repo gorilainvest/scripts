a1864442836c881707ed1ad6a5e3be1464539dd5
Thompson Filgueiras <tigredonorte3@gmail.com>
2018-01-19 16:59:24 -0200

    basic ci commands

---
+++ ci/ci-develop.sh
@@ -0,0 +1,3 @@
+cd ..
+npm run version
+git commit -m "version"
---
+++ ci/ci-setup.sh
@@ -0,0 +1,9 @@
+nvm install 6.9.1
+node --version
+npm --version
+npm install -g @angular/cli
+cd app
+npm update
+npm install --unsafe-perm
+export FIREFOX_VERSION=52.0
+curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/firefox.sh | bash -s
